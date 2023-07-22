d14d8f68871cc6170157c52a589e627218149909
Muhammad Majid Latif <mlmajid.latif1145@gmail.com>
2023-07-22 10:36:30 -0700

    second commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,56 +1,86 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedTab = 0;
+  final List<Widget> pages = [
+    Center(child: Text('Events Screen')),
+    Center(child: Text('Marketplace Screen')),
+    Center(child: Text('Activity Screen')),
+    Center(child: Text('Notifications Screen')),
+    Center(child: Text('Profile Screen')),
+  ]; // Screens
+
+  changeTab(int index) {
+    setState(() {
+      selectedTab = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final List<Tab> myTabs = [
-      Tab(icon: Image.asset('Assets/events.png')),
-      Tab(icon: Image.asset('Assets/marketplace.png')),
-      Tab(icon: Image.asset('Assets/activity.png')),
-      Tab(icon: Image.asset('Assets/notifications.png')),
-      Tab(icon: Image.asset('Assets/me.png')),
-    ];
-    return DefaultTabController(
-      length: myTabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-          title: Text(
-            "TOGOPARTS",
-            style: TextStyle(
-                color: Colors.orange,
-                fontWeight: FontWeight.bold,
-                fontSize: 30),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        title: Text(
+          "TOGOPARTS",
+          style: TextStyle(
+            color: Colors.orange,
+            fontWeight: FontWeight.bold,
+            fontSize: 40,
           ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Image.asset("Assets/shopping_bag.png"),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Image.asset("Assets/message.png"),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Image.asset("Assets/menubar.png"),
-            ),
-          ],
         ),
-        body: TabBarView(children: [
-          Center(child: Text('Tab 1 Content')),
-          Center(child: Text('Tab 2 Content')),
-          Center(child: Text('Tab 3 Content')),
-          Center(child: Text('Tab 4 Content')),
-          Center(child: Text('Tab 5 Content')),
-        ]),
-        bottomNavigationBar: TabBar(
-          tabs: myTabs,
-          padding: EdgeInsets.all(10),
-        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset("Assets/shopping_bag.png"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset("Assets/message.png"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset("Assets/menubar.png"),
+          ),
+        ],
+      ),
+      body: pages[selectedTab],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedTab,
+        onTap: (index) => changeTab(index),
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('Assets/events.png'),
+            label: "Events",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('Assets/marketplace.png'),
+            label: "Marketplace",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('Assets/activity.png'),
+            label: "Activity",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('Assets/notifications.png'),
+            label: "Notifications",
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('Assets/me.png'),
+            label: "Me",
+          ),
+        ],
       ),
     );
+    // );
   }
 }
